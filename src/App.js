1265d72f6817aa4c30e10bddfc051e567f6e09c4
chris artrip <christopherartrip@gmail.com>
2021-01-13 14:31:17 -0500

    Grabbed API key

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
+import GifFinder from 'GifFinder'
 import './App.css';
-
+ //eZETetqB8EQ0l0Zr2WBvmigHLqypFLif      <--- thats the API key
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <GifFinder />
     </div>
   );
 }
---
+++ src/components/GifFinder.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function GifFinder() {
+    return (
+        <div>
+            this is the gifFinder page.
+        </div>
+    )
+}
+
