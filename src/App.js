2c11d0269ea9833fc90bf191161372c38e3f2a46
chris artrip <christopherartrip@gmail.com>
2021-01-13 15:08:56 -0500

    Fetched the data from API key

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import GifFinder from './components/GifFInder'
 import './App.css';
- //g25nBHy2WQJRvUjil4QWVNdHBBEfeYEH      <--- thats the API key
+ //http://api.giphy.com/v1/gifs/trending?api_key=g25nBHy2WQJRvUjil4QWVNdHBBEfeYEH      <--- Trending Search
+ //http://api.giphy.com/v1/gifs/search?q=SEARCH+TERM+GOES+HERE&api_key=g25nBHy2WQJRvUjil4QWVNdHBBEfeYEH      <---
 function App() {
   return (
     <div className="App">
--- src/components/GifFinder.js
+++ src/components/GifFinder.js
@@ -1,6 +1,28 @@
 import React, { Component } from 'react'
 
 export class gifFinder extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            gifUrl: [],
+            link: ''
+        }
+    }
+
+    fetchUrl = async () => {
+        try {
+            const url = 'http://api.giphy.com/v1/gifs/trending?api_key=g25nBHy2WQJRvUjil4QWVNdHBBEfeYEH';
+            const response = await fetch(url);
+            console.log(response)
+            if(response.status !== 200) {
+                throw new Error("Error");
+            }
+            const data = await response.json();
+        } catch(error) {
+            console.log(error)
+        }
+    }
+
     render() {
         return (
             <div>
