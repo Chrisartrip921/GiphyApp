9f28086685095067dce1204c2b48a04f2e8a7529
chris artrip <christopherartrip@gmail.com>
2021-01-13 20:10:08 -0500

    new component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import React from 'react'
 import GifFinder from './components/GifFInder'
 import './App.css';
  //http://api.giphy.com/v1/gifs/trending?api_key=g25nBHy2WQJRvUjil4QWVNdHBBEfeYEH      <--- Trending Search
  //http://api.giphy.com/v1/gifs/search?q=SEARCH+TERM+GOES+HERE&api_key=g25nBHy2WQJRvUjil4QWVNdHBBEfeYEH      <---
+ 
 function App() {
+  
   return (
     <div className="App">
       <GifFinder />
--- src/components/GifFinder.js
+++ src/components/GifFinder.js
@@ -1,38 +1,21 @@
-import React, { Component } from 'react'
+import {useEffect, useState} from 'react';
+import axios from 'axios';
 
-export class gifFinder extends Component {
-    constructor(props){
-        super(props)
-        this.state = {
-            gifUrl: [],
-            link: ''
-        }
-    }
-
-    fetchUrl = async () => {
-        try {
-            const url = 'http://api.giphy.com/v1/gifs/trending?api_key=g25nBHy2WQJRvUjil4QWVNdHBBEfeYEH';
-            const response = await fetch(url);
-            console.log(response)
-            if(response.status !== 200) {
-                throw new Error("Error");
-            }
-            const data = await response.json();
-        } catch(error) {
-            console.log(error)
-        }
-    }
-
-    render() {
-        return (
-            <div>
-                thiis is the gifFinderPage
-            </div>
-        )
-    }
+const GifFInder = () => {
+    const [data, setData] = useState([]);
+    useEffect(() => {
+        const fetchData = async () => {
+            const results = await axios("http://api.giphy.com/v1/gifs/trending", {
+                params: {
+                    api_key: "g25nBHy2WQJRvUjil4QWVNdHBBEfeYEH"
+                }
+            })
+            console.log(results)
+            setData(results.data.data)
+        };
+        fetchData();
+    }, []);
+    return <div>Hello</div>
 }
 
-export default gifFinder
-
-
-
+export default GifFInder
